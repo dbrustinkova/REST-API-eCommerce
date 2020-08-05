01154edb54d881ed0372df3c9aabbed255ab5911
dbrustinkova <dlbrustinkova@gmail.com>
2020-08-05 20:21:58 +0300

    Add files via upload

---
+++ Shared/Enums/Statuses.cs
@@ -0,0 +1,14 @@
+﻿namespace AppGr8.WebApiECommerce.DataAccess.Enums
+{
+    /// <summary>
+    /// Statuses for orders (not used)
+    /// </summary>
+    public enum Statuses
+    {
+        New,
+        Payment,
+        Delivery,
+        Cancelled,
+        Completed
+    }
+}
